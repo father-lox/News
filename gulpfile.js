0dcfe85b2811f90df6f431e7c6d19d928bf29560
Your Name <you@example.com>
2022-04-09 07:17:45 +0300

    add font processing

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ import del from 'del';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import fileinclude from 'gulp-file-include';
+import fs from 'fs';
+import gulpFonter from 'gulp-fonter';
+import ttf2woff2 from 'gulp-ttf2woff2';
 import { path } from './gulp/config/path.js';
 
 const sass = gulpSass(dartSass);
@@ -31,7 +34,30 @@ function compileHtml() {
         .pipe(gulp.dest(path.build.htmlFiles))
 }
 
-const fileProcessing = gulp.parallel(compileHtml, compileSass);
+function convertOtfToTtf() {
+    return gulp.src(`${path.source.fonts}/*.otf`)
+        .pipe(gulpFonter({
+            formats: ['ttf']
+        }))
+        .pipe(gulp.dest(path.build.fonts));
+}
+
+function convertTtfToWoff() {
+    return gulp.src(`${path.source.fonts}/*.ttf`)
+        .pipe(gulpFonter({
+            formats: ['woff']
+        }))
+        .pipe(gulp.dest(`${path.build.fonts}/*/`))
+}
+
+function convertOtfToWoff2() {
+    return gulp.src(`${path.source.fonts}/*.ttf`)
+        .pipe(ttf2woff2())
+        .pipe(gulp.dest(path.build.fonts));
+}
+
+const fontsConverter = gulp.series(convertOtfToTtf, convertTtfToWoff, convertOtfToWoff2);
+const fileProcessing = gulp.parallel(fontsConverter, compileHtml, compileSass);
 const devActions = gulp.series(reset, fileProcessing, watching);
 
 gulp.task('default', devActions);
