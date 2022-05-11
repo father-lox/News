e4d7fe81482ade241e0723952130ddf06f76999f
Semen <bethesda.stillxxxx@gmial.com>
2022-05-11 14:14:57 +0300

    done

--- client/server/application.ts
+++ client/server/application.ts
@@ -1,11 +1,9 @@
 import express, {Router} from "express"
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import cors from "cors"
 import { engine } from "express-handlebars"
 import * as path from "path";
 import hbs from "hbs"
-import nodeSassMiddleware from "node-sass-middleware";
 
 export default (root: Router) => {
     const app = express()
@@ -14,18 +12,14 @@ export default (root: Router) => {
     app.use(bodyParser.json())
     app.use(cookieParser())
 
-    hbs.registerPartials(path.join(__dirname, 'components'))
+    hbs.registerPartials(path.join(__dirname, '..','components'))
     app.set('view engine', 'handlebars')
-    app.use(nodeSassMiddleware({
-        src: path.join(__dirname, 'pages', 'index'),
-        dest: path.join(__dirname, 'public'),
-        outputStyle: "compressed"
-    }))
     app.engine('handlebars', engine())
-    app.set('views', path.join(__dirname, 'pages'))
+    app.set('views', path.join(__dirname, '..', 'pages'))
+
+    app.use(express.static(path.join(__dirname, '..', 'pub')))
 
     app.use('/', root)
-    app.use(express.static(path.join(__dirname)))
 
     // Error handler
     // app.use()
