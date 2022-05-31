63b9d85d9b01e79fae3b7c5f6c376c71459a99bd
Your Name <bethesda.stillxxxx@gmail.com>
2022-05-31 15:44:24 +0300

    added render of random news

--- client/server/index.ts
+++ client/server/index.ts
@@ -3,4 +3,4 @@ import route from "./src/route/root";
 import application from "./application";
 const app = application(route)
 
-app.listen(3000, () => console.log('Server started...'))
+app.listen(3500, () => console.log('Server started...'))
--- client/server/src/route/root.ts
+++ client/server/src/route/root.ts
@@ -4,9 +4,9 @@ import fetch from "node-fetch";
 const route = Router()
 
 route.get('/', async (req, res) => {
-    let result = await fetch('http://localhost:4000/api/news').then(r => r.json())
-    let context = result[1]
-    context.commentCount =context.Comments.length
+    let result = await fetch('http://localhost:4000/api/news/random').then(r => r.json())
+    let context = result
+    context.commentCount = context.Comments.length
     res.render('index/index.hbs', {layout: false, context: context})
 })
 
