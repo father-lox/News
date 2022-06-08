58205fd6345abda7b461f5df26b4d548ca0dac5b
Your Name <you@example.com>
2022-06-08 23:04:22 +0300

    FIx "show comment" button

--- client/components/news/code/news.ts
+++ client/components/news/code/news.ts
@@ -2,13 +2,11 @@ const selectorBody: string = 'body';
 const selectorButtonShowComments: string = '.news [data-role=news-comment-button]';
 const domBody: HTMLBodyElement = document.querySelector(selectorBody) as HTMLBodyElement;
 
-domBody.addEventListener('click', (event) : void => {
-    const button: HTMLButtonElement = (event.target as HTMLElement).closest(selectorButtonShowComments) as HTMLButtonElement;
-
-    if (!button) {
-        return;
-    }
-
-    const comments: HTMLDivElement = document.querySelector('.news [data-role=news-comments-section]') as HTMLDivElement;
-    comments.classList.toggle('hidden')
+document.querySelectorAll(selectorButtonShowComments).forEach(button => {
+    button.addEventListener('click', (event) : void => {
+        const newsDiv: HTMLDivElement = (event.target as HTMLElement).closest('.news') as HTMLDivElement;
+        const comments: HTMLDivElement = newsDiv.querySelector('.news [data-role=news-comments-section]') as HTMLDivElement;
+    
+        comments.classList.toggle('hidden')
+    });
 });
